a3fe651792f3af9b14bb8edae0ca623f32db2055
Rodney Abad Arocha <r.abadarocha@gmail.com>
2020-07-01 22:57:30 -0300

    cleaning

--- gamification-server 0/public/routes/mixerRouter.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var mixerController = require('../controllers/mixerController');
-
-router.get('/calcular', mixerController.calcular);
-
-module.exports = router;
